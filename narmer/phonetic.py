8988b7c0dd689fb0c7eb08855ee1e8ee786baaa5
Chris Little <chrisclittle@gmail.com>
2015-04-22 18:03:24 -0700

    corrected name

--- narmer/phonetic.py
+++ narmer/phonetic.py
@@ -5,7 +5,7 @@ The phonetic module implements phonetic algorithms including:
 
 
 Copyright 2015 by Christopher C. Little.
-This file is part of Abydos.
+This file is part of Narmer.
 
 Narmer is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
