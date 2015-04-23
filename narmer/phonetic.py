7f0de774cd5082ac1a01c726589d0661716337cf
Chris Little <chrisclittle@gmail.com>
2015-04-22 17:54:54 -0700

    added stubs for phonetic module

---
+++ narmer/phonetic.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""narmer.phonetic
+
+The phonetic module implements phonetic algorithms including:
+
+
+Copyright 2015 by Christopher C. Little.
+This file is part of Abydos.
+
+Narmer is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Narmer is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Narmer. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from __future__ import unicode_literals
+from __future__ import division
