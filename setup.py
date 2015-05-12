44f347da2a150ee9bd0cab8ac550f9a4039da945
Chris Little <chrisclittle@gmail.com>
2015-05-11 17:28:01 -0700

    setup file somewhat updated; some PEP8 fixes applied

--- setup.py
+++ setup.py
@@ -1,26 +1,41 @@
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+
+def readfile(fn):
+    """Read fn and return the contents."""
+    with open(path.join(here, fn), 'r', encoding='utf-8') as f:
+        return f.read()
+
 setup(
-      name = 'narmer',
-      packages = ['narmer'],
-      version = '0.1.0',
-      description = 'Narmer Experimental NLP/IR library for Python',
-      author = 'Christopher C. Little',
-      author_email = 'chrisclittle@gmail.com',
-      url = 'https://github.com/chrislit/narmer',
-      keywords = ['nlp', 'ai', 'ir'],
-      classifiers = [
-                     'Programming Language :: Python',
-                     'Programming Language :: Python :: 3',
-                     'Development Status :: 2 - Pre-Alpha',
-                     'Environment :: Other Environment',
-                     'Intended Audience :: Developers',
-                     'Operating System :: OS Independent',
-                     'Topic :: Scientific/Engineering :: Artificial Intelligence',
-                     'Topic :: Software Development :: Libraries :: Python Modules',
-                     'Topic :: Text Processing :: Indexing',
-                     'Topic :: Text Processing :: Linguistic',
-                     ],
-      long_description = """Narmer Experimental NLP/IR package"""
+      name='narmer',
+      packages=['narmer'],
+      version='0.1.0',
+      description='Narmer Experimental NLP/IR library for Python',
+      author='Christopher C. Little',
+      author_email='chrisclittle@gmail.com',
+      url='https://github.com/chrislit/narmer',
+      keywords=['nlp', 'ai', 'ir', 'language', 'linguistics',
+                'phonetic algorithms'],
+      classifiers=[
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 3',
+                   'Development Status :: 2 - Pre-Alpha',
+                   'Environment :: Other Environment',
+                   'Intended Audience :: Developers',
+                   'Operating System :: OS Independent',
+                   'Topic :: Scientific/Engineering :: Artificial Intelligence',
+                   'Topic :: Software Development :: Libraries :: Python Modules',
+                   'Topic :: Text Processing :: Indexing',
+                   'Topic :: Text Processing :: Linguistic',
+                   ],
+      long_description='\n\n'.join([readfile(f) for f in ('README.rst',
+                                                          'HISTORY.rst',
+                                                          'AUTHORS.rst')]),
+      install_requires=['abydos'],
       )
