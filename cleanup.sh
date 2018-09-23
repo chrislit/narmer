b2efe85f87dcde2fa9c6545059dc3b43b99a73ec
Chris Little <chrisclittle@gmail.com>
2018-09-22 22:03:39 -0700

    added to cleanup (based on abydos)

--- cleanup.sh
+++ cleanup.sh
@@ -2,9 +2,11 @@
 
 rm -rf ./cover
 rm -rf ./flake8
-rm -rf ./abydos.egg-info
+rm -rf ./narmer.egg-info
 rm -rf ./dist
 rm -rf ./build
+rm -rf ./.tox
+rm -rf ./.mypy_cache
 rm -rf ./*.log
 rm -rf ./.coverage
 rm -rf */*.pyc
